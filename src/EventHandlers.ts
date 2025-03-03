9faa2087927e2ed14c85206257828c560461c48f
denhampreen <denhampreen@gmail.com>
2025-03-03 18:06:49 +0200

    hackerman: using this indexer deployment to sync a hyper instance, will revert later

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -1,13 +1,19 @@
-/*
- * Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features
- */
 import {
-  SafeProxyFactory,
+  ECR20,
+  ECR20_Approval,
 } from "generated";
 
-SafeProxyFactory.ProxyCreation.contractRegister(({ event, context }) => {
-  context.addSafeProxy(event.params.proxy);
-},  {
-  preRegisterDynamicContracts: true
-});
+ECR20.Approval.handler(async ({ event, context }) => {
 
+  const entity: ECR20_Approval = {
+    id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    owner: event.params.owner,
+    spender: event.params.spender,
+    value: event.params.value,
+    token: event.srcAddress,    
+    hash: event.transaction.hash,
+  };
+  
+
+  context.ECR20_Approval.set(entity);
+}, {wildcard: true});
