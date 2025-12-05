bbc82d06965493dc9a62aa310cbfae57a8e8749a
denhampreen <denhampreen@gmail.com>
2025-12-05 18:24:41 +0200

    chore: strip api to safes, chainId & owners

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,7 @@
-import exp from "constants";
-
 export const addOwner = async (event: any, context: any) => {
   const { owner } = event.params;
   const { srcAddress, chainId } = event;
-  const safeId = chainId+"-"+srcAddress;
+  const safeId = chainId + "-" + srcAddress;
 
   const safe = await context.Safe.get(safeId);
 
@@ -14,8 +12,6 @@ export const addOwner = async (event: any, context: any) => {
     context.Safe.set({
       ...safe,
       owners: [...safe.owners, owner],
-      numberOfOwners: safe.numberOfOwners + 1,
-      thresholdOwnerRatio: safe.threshold / safe.numberOfOwners + 1,
     })
   }
 };
@@ -23,7 +19,7 @@ export const addOwner = async (event: any, context: any) => {
 export const removeOwner = async (event: any, context: any) => {
   const { owner } = event.params;
   const { srcAddress, chainId } = event;
-  const safeId = chainId+"-"+srcAddress;
+  const safeId = chainId + "-" + srcAddress;
 
   const safe = await context.Safe.get(safeId);
 
@@ -33,47 +29,8 @@ export const removeOwner = async (event: any, context: any) => {
   } else {
     context.Safe.set({
       ...safe,
-      owners: safe.owners.filter( (o : string) => o !== owner),
-      numberOfOwners: safe.numberOfOwners - 1,
-      thresholdOwnerRatio: safe.threshold / safe.numberOfOwners - 1,
+      owners: safe.owners.filter((o: string) => o !== owner),
     })
   }
 };
 
-export const executionSuccess = async (event: any, context: any) => {
-  const { payment } = event.params;
-  const { srcAddress, chainId } = event;
-  const safeId = chainId+"-"+srcAddress;
-
-  const safe = await context.Safe.get(safeId);
-
-  if (!safe) {
-    context.log.warn(`Safe not found for ${safeId}`);
-    return;
-  } else {
-    context.Safe.set({
-      ...safe,
-      numberOfSuccessfulExecutions: safe.numberOfSuccessfulExecutions + 1,
-      totalGasSpent: safe.totalGasSpent + payment,
-    })
-  }
-}
-
-export const executionFailure = async (event: any, context: any) => {
-  const { payment } = event.params;
-  const { srcAddress, chainId } = event;
-  const safeId = chainId+"-"+srcAddress;
-
-  const safe = await context.Safe.get(safeId);
-
-  if (!safe) {
-    context.log.warn(`Safe not found for ${safeId}`);
-    return;
-  } else {
-    context.Safe.set({
-      ...safe,
-      numberOfFailedExecutions: safe.numberOfFailedExecutions + 1,
-      totalGasSpent: safe.totalGasSpent + payment,
-    })
-  }
-}
