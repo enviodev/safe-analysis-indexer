e51ae6b27821f575621e187f6310b7d21f1eaf39
denhampreen <denhampreen@gmail.com>
2026-01-30 08:27:28 +0200

    fix: build errors

--- explorer/src/lib/graphql/client.ts
+++ explorer/src/lib/graphql/client.ts
@@ -4,8 +4,13 @@ import { GRAPHQL_ENDPOINT } from "../constants";
 export const graphqlClient = new GraphQLClient(GRAPHQL_ENDPOINT, {
   // Disable Next.js fetch caching for live data
   fetch: (url, options) => 
+<<<<<<< HEAD
     fetch(url, { 
       ...options, 
+=======
+    fetch(url as string, { 
+      ...options as RequestInit, 
+>>>>>>> 2be53bd (fix: build errors)
       cache: "no-store",
       next: { revalidate: 0 }
     }),
